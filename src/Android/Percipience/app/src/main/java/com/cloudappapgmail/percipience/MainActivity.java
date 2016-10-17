34641e7af19c894d5448b972cc71d8865773eac5
Jeroen <jeroenrietveld482@gmail.com>
2016-10-17 12:11:48 +0200

    Added reading data from Firebase function

--- src/Android/Percipience/app/src/main/java/com/cloudappapgmail/percipience/MainActivity.java
+++ src/Android/Percipience/app/src/main/java/com/cloudappapgmail/percipience/MainActivity.java
@@ -3,11 +3,31 @@ package com.cloudappapgmail.percipience;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+         Firebase.setAndroidContext(this);
+        Firebase myFirebaseRef = new Firebase("https://percipience-ace91.firebaseio.com/");
+        System.out.println("hey");
+        myFirebaseRef.child("message").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                System.out.println(snapshot.getValue());  //prints "Do you have data? You'll love Firebase."
+            }
+            @Override public void onCancelled(FirebaseError error) { }
+        });
+
+        myFirebaseRef.child("message").setValue("Do you have data? You'll love Firebase.");
     }
+
+
+
 }
