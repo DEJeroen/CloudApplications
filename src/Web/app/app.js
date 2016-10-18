bb1610ba52ce620e8b9c7ee4ef5e266fa382f6e3
Boyd220 <boydfranken@msn.com>
2016-10-18 14:31:54 +0200

    Added the first view with working controller and added routing through views. Also added the neccesary cdns

--- src/Web/app/app.js
+++ src/Web/app/app.js
@@ -1,9 +1,21 @@
-var app = angular.module("myapp",[]);
-app.controller("mainCtrl", function($scope, $http){
+var app = angular.module("myapp",['ngRoute', 'ngAnimate']);
+app.config(['$routeProvider',
+  function ($routeProvider) {
+        $routeProvider
+            .when('/createLesson', {
+                title: 'CreateLesson',
+                templateUrl: 'view/createLesson.html',
+                controller: 'createLessonCtrl',
+            })
+                        .otherwise({
+                redirectTo: '/createLesson'
+            });
+    }]);
+
+app.controller("createLessonCtrl", function($scope, $http){
     
-    $scope.Greet = function() {
-        console.log("Hello World")
-         };
+console.log("hallo");
 
 });
+
 
