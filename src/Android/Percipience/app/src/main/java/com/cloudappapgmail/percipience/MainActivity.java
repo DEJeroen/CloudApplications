15e10104f50297d96af4b7ffbda2f556f67f3a20
DEJeroen <jeroenrietveld482@gmail.com>
2016-10-18 16:06:10 +0200

    Added detect selected lesson on the Android device

--- src/Android/Percipience/app/src/main/java/com/cloudappapgmail/percipience/MainActivity.java
+++ src/Android/Percipience/app/src/main/java/com/cloudappapgmail/percipience/MainActivity.java
@@ -2,6 +2,7 @@ package com.cloudappapgmail.percipience;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
@@ -10,22 +11,32 @@ import com.firebase.client.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
+    String currentLesson;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
          Firebase.setAndroidContext(this);
+
+
         Firebase myFirebaseRef = new Firebase("https://percipience-ace91.firebaseio.com/");
         System.out.println("hey");
-        myFirebaseRef.child("message").addValueEventListener(new ValueEventListener() {
+        myFirebaseRef.child("ID1/Lessen/GeselecteerdeLes").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot snapshot) {
-                System.out.println(snapshot.getValue());  //prints "Do you have data? You'll love Firebase."
+                currentLesson = snapshot.getValue().toString();
+                TextView textViewCurrentLesson = (TextView)findViewById(R.id.textViewCurrentLesson);
+                textViewCurrentLesson.setText(currentLesson);
+
+                System.out.println(currentLesson);  //prints "Do you have data? You'll love Firebase."
             }
             @Override public void onCancelled(FirebaseError error) { }
         });
 
-        myFirebaseRef.child("message").setValue("Do you have data? You'll love Firebase.");
+        //
+
+    //    myFirebaseRef.child("message").setValue("Do you have data? You'll love Firebase.");
     }
 
 
