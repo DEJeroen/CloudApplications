e02ca254f33873f02cd71d43a8375d5a8713eef5
Boyd220 <boydfranken@msn.com>
2016-11-24 13:28:45 +0100

    Added new node module, added test graph

--- src/Web/app/app.js
+++ src/Web/app/app.js
@@ -38,6 +38,11 @@ app.config(['$routeProvider',
                 templateUrl: 'view/startLessons/viewquestion.html',
                 controller: 'viewquestionCtrl',
             })
+            .when('/viewGraph', {
+                title: 'viewGraph',
+                templateUrl: 'view/makingGraph/graphTest.html',
+                controller: 'graphTestCtrl',
+            })
                                 .otherwise({
                 redirectTo: '/createLessonKlas'
             });
@@ -252,7 +257,6 @@ app.controller("startLessonSubjectCtrl", function($scope, $http, $location, Data
 });
 
 app.controller("viewquestionCtrl", function($scope, $http, $location, DataService){
-    $scope.data = DataService.getProducts();
     
 
     console.log($scope.data);
@@ -294,3 +298,42 @@ app.service('DataService', function() {
   };
 });
 //Eind van controllers voor les geven
+app.controller("graphTestCtrl", function($scope, $http, $location){
+var ctx = document.getElementById("myChart");
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 4, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
+});
