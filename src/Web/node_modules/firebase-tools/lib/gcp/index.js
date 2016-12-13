776eca246c31b27133eed0729c10e62fdb9e3446
Boyd220 <boydfranken@msn.com>
2016-12-13 13:45:36 +0100

    Another small change

--- src/Web/node_modules/firebase-tools/lib/gcp/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = {
-  cloudlogging: require('./cloudlogging'),
-  cloudfunctions: require('./cloudfunctions'),
-  storage: require('./storage'),
-  pubsub: require('./pubsub'),
-  apikeys: require('./apikeys')
-};
